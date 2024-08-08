6ccd4ef480fd9313b7914bf03152f876fe640aa2
Letícia Castelo <leticiacastelo.dev@gmail.com>
2024-08-07 23:51:06 -0300

    Models config

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
+class Brand(models.Model):
+  id = models.AutoField
+  
 class Car(models.Model):
   id = models.AutoField(primary_key=True)
   model = models.CharField(max_length=200)
@@ -7,3 +11,7 @@ class Car(models.Model):
   factory_year = models.IntegerField(blank=True, null=True)
   model_year = models.BigIntegerField(blank=True, null=True)
   value = models.FloatField(blank=True, null=True)
+
+  def __str__(self):
+    return self.model
+  
