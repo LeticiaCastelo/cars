91971a09128602bfa1bca28ef3fd269b3ebdfe76
Letícia Castelo <leticiacastelo.dev@gmail.com>
2024-09-03 21:44:31 -0300

    model__contains=search adicionado

--- cars/views.py
+++ cars/views.py
@@ -3,10 +3,15 @@ from cars.models import Car
 
 
 def cars_view(request):
-  cars = Car.objects.all()
+    cars = Car.objects.all()
+    search = request.GET.get('search','')
+
+    if search:
+        cars = Car.objects.filter(model__contains=search)
+
   
-  return render(
-    request, 
-    'cars.html', 
-    {'cars': cars }
+    return render(
+        request, 
+        'cars.html', 
+        {'cars': cars }
     )
